57c57675c626ddce343c5d27b847e3ea96c4ef78
zoro <zoroxdp@gmail.com>
2024-06-29 16:41:29 +0530

    Commit-V1.5

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -3,7 +3,7 @@ export const Quote = () => {
   return <div className="bg-cyan-200 h-screen flex flex-col justify-center">
     <div className="flex justify-center">
       <div className="flex flex-col justify-around">
-        <DotLottieReact className="-mt-56" src="/src/assets/ship.lottie" loop autoplay />
+        <DotLottieReact className="-mt-56" src="public/assets/ship.lottie" loop autoplay />
         <div className="mt-4 px-5 mx-10 md:text-xl lg:text-2xl font-semibold">
           Welcome onboard,<br /><br />
           Thrilled to have you here. Whether you’re a seasoned reader or a curious wanderer, this space is all about sharing ideas, stories, and musings. Dive into the archives and explore the latest posts.<br />
