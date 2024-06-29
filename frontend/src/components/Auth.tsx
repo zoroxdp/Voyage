f2ff2cc0e6fef5ee9e8c3fb9b321c782b6ba54e6
zoro <zoroxdp@gmail.com>
2024-06-29 20:58:11 +0530

    Commit-V1.7

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -27,42 +27,44 @@ export const Auth = ({ authType }: { authType: "signup" | "signin" }) => {
     }
   }
 
-  return <div className="h-screen flex flex-col justify-between">
-    <div className="mt-32">
-      <div className="flex justify-center items-center text-center">
-        <div>
-          <img className="w-48 h-48 mx-auto" src="/assets/Voyage.png" alt="logo" />
-          <div className="text-4xl font-bold px-20">
-            {authType === 'signup' ? 'Create Account' : 'Login to Voyage'}
-          </div>
-          <div className="text-md text-slate-400 font-normal">
-            {authType === 'signup' ? 'Already have an account?' : 'Create your account'}
-            {authType === 'signup' ?
-              <Link to={"/signin"} className="pl-1 underline text-sky-500">Sign In</Link> :
-              <Link to={"/signup"} className="pl-1 underline text-sky-500">Sign Up</Link>
-            }
-          </div>
-          <div className="pt-5 text-left">
-            {authType === 'signup' ?
-              <LabeledInput label="Name" type="" placeholder="Enter your Name" onChange={(e) => {
-                setPostInputs({ ...postInputs, name: e.target.value });
-              }} /> : ""
-            }
-            <LabeledInput label="Email" type="" placeholder="Enter your Email" onChange={(e) => {
-              setPostInputs({ ...postInputs, email: e.target.value });
-            }} />
-            <LabeledInput label="Password" type="password" placeholder="Enter your Password" onChange={(e) => {
-              setPostInputs({ ...postInputs, password: e.target.value });
-            }} />
-            <button type="button" onClick={sendRequest} className="my-6 w-full text-white bg-slate-950 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xl px-5 py-3.5 text-center">{authType === 'signup' ? 'Sign Up' : 'Sign In'}</button>
+  return (
+    <div className="h-screen flex flex-col justify-between">
+      <div className="mt-32">
+        <div className="flex justify-center items-center text-center">
+          <div>
+            <img className="w-48 h-48 mx-auto" src="/assets/Voyage.png" alt="logo" />
+            <div className="text-4xl font-bold px-20">
+              {authType === 'signup' ? 'Create Account' : 'Login to Voyage'}
+            </div>
+            <div className="text-md text-slate-400 font-normal">
+              {authType === 'signup' ? 'Already have an account?' : 'Create your account'}
+              {authType === 'signup' ?
+                <Link to={"/signin"} className="pl-1 underline text-sky-500">Sign In</Link> :
+                <Link to={"/signup"} className="pl-1 underline text-sky-500">Sign Up</Link>
+              }
+            </div>
+            <div className="pt-5 text-left">
+              {authType === 'signup' ?
+                <LabeledInput label="Name" type="" placeholder="Enter your Name" onChange={(e) => {
+                  setPostInputs({ ...postInputs, name: e.target.value });
+                }} /> : ""
+              }
+              <LabeledInput label="Email" type="" placeholder="Enter your Email" onChange={(e) => {
+                setPostInputs({ ...postInputs, email: e.target.value });
+              }} />
+              <LabeledInput label="Password" type="password" placeholder="Enter your Password" onChange={(e) => {
+                setPostInputs({ ...postInputs, password: e.target.value });
+              }} />
+              <button type="button" onClick={sendRequest} className="my-6 w-full text-white bg-slate-950 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xl px-5 py-3.5 text-center">{authType === 'signup' ? 'Sign Up' : 'Sign In'}</button>
+            </div>
           </div>
         </div>
       </div>
+      <div>
+        <DotLottieReact className="block lg:hidden object-fill -mt-80 w-screen" src="/src/assets/ship.lottie" loop autoplay />
+      </div>
     </div>
-    <div>
-      <DotLottieReact className="block lg:hidden object-fill -mt-80 w-screen" src="/src/assets/ship.lottie" loop autoplay />
-    </div>
-  </div>
+  )
 }
 
 function LabeledInput({ label, placeholder, type, onChange }: { label: string, placeholder: string, type: string, onChange: (e: any) => void }) {
